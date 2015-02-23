3a392b13fc3e7419aa73a52b068479921d9e7ccb
Michael Angelo <yamikamisama@gmail.com>
2015-02-23 15:52:24 -0500

    add app1 and Angular Hello World

---
+++ App1/app.js
@@ -0,0 +1,19 @@
+var app = angular.module('app', []);
+
+// var MainController = function($scope) {
+// 	$scope.val = "test123"
+// }
+
+app.controller('MainController', function($scope){
+	$scope.val = 1
+	$scope.even = false
+
+	$scope.inc = function(){
+		$scope.val += 1
+		$scope.even = $scope.val%2==0
+	}
+});
+
+// angular.module('controllers', []).controller('MainController', function($scope){
+// 	$scope.val = "test789"
+// })
