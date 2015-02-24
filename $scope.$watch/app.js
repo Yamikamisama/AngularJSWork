a9c284c968dbbdf7e86ece6ba4ed8c212e6338c8
Michael Angelo <yamikamisama@gmail.com>
2015-02-24 14:41:10 -0500

    add part 2 of . with break function

--- $scope.$watch/app.js
+++ $scope.$watch/app.js
@@ -1,12 +1,25 @@
 var app = angular.module('app', []);
 
-app.controller('MainController', function($scope){
-	$scope.mydata = {val: "Michael"}
+// app.controller('MainController', function($scope){
+// 	$scope.mydata = {val: "Michael"}
 
-	$scope.$watch('mydata.val', function(newval){
-		$scope.mydata.toolong = newval.length > 15
-	})
-});
+// 	$scope.$watch('mydata.val', function(newval){
+// 		$scope.mydata.toolong = newval.length > 15
+// 	})
+// });
 
 // very powerful too, it requires know when and when not to use it.
 // keep your code DRY
+
+app.controller('MainController', function($scope){
+	$scope.num = 0;
+	$scope.nums = [];
+
+	$scope.increment = function(){
+		$scope.num++;
+	}
+
+	$scope.breakit = $scope.$watch('num', function(){
+		$scope.nums.push($scope.num)
+	});
+});
